0820f725d3f77808261766d6d22a1b3c8a0745eb
Omonov <algoritm1202@gmail.com>
2025-01-10 20:45:54 +0500

    fixsed meta data

--- app/profile/logout/page.jsx
+++ app/profile/logout/page.jsx
@@ -1,8 +1,31 @@
 import LogoutBox from "@/components/profile/LogOutBox";
 
 export const metadata = {
-  title: "Profile Bo'limi",
-  description: "Game PromoCode",
+  title: "Uzpin - promo codes for the game",
+  description:
+    "Promo codes for PUBG Mobile Legends, Free Fire, Steam and many other games and apps",
+  openGraph: {
+    title: "Uzpin - promo codes for the game",
+    description:
+      "Promo codes for PUBG Mobile Legends, Free Fire, Steam and many other games and apps",
+    url: "https://uzpin.games",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 300,
+        height: 300,
+        alt: "Uzpin Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Uzpin - promo codes for the game",
+    description:
+      "Promo codes for PUBG Mobile Legends, Free Fire, Steam and many other games and apps",
+    images: ["/favicon.ico"],
+  },
 };
 export default function Logout() {
   return <LogoutBox />;
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,30 @@
 import ProfilInfo from "@/components/profile/ProfileInfo";
-export const Metadata = {
-  title: "Profile Bo'limi",
-  description: "Game PromoCode",
+export const metadata = {
+  title: "Uzpin - promo codes for the game",
+  description:
+    "Promo codes for PUBG Mobile Legends, Free Fire, Steam and many other games and apps",
+  openGraph: {
+    title: "Uzpin - promo codes for the game",
+    description:
+      "Promo codes for PUBG Mobile Legends, Free Fire, Steam and many other games and apps",
+    url: "https://uzpin.games",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 300,
+        height: 300,
+        alt: "Uzpin Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Uzpin - promo codes for the game",
+    description:
+      "Promo codes for PUBG Mobile Legends, Free Fire, Steam and many other games and apps",
+    images: ["/favicon.ico"],
+  },
 };
 export default function Profil() {
   return (
