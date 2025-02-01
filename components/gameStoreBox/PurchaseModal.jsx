c0ef0a13d75f2dd44fe47fd5bb596aa5fec6101c
Omonov <algoritm1202@gmail.com>
2025-02-01 11:30:20 +0500

    push

--- components/gameStoreBox/PurchaseModal.jsx
+++ components/gameStoreBox/PurchaseModal.jsx
@@ -111,7 +111,7 @@ export function PurchaseModal({
 
   return (
     <div
-      className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      className="fixed inset-0 z-50 rounded-[10px] bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
       open={isOpen}
     >
       {error2 && (
