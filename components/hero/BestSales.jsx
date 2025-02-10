c7c19b006ccfd6bfffed3dc521fedb0694549aed
Omonov <algoritm1202@gmail.com>
2025-02-10 11:16:13 +0500

    push

--- components/hero/BestSales.jsx
+++ components/hero/BestSales.jsx
@@ -44,7 +44,12 @@ export default function BestSales() {
   }, []);
 
   const formatNumber = (num) => {
-    return num % 1 === 0 ? num?.toFixed(0) : num?.toFixed(2);
+    const str = num.toString();
+    if (str.includes(".")) {
+      const [integerPart, decimalPart] = str.split(".");
+      return `${integerPart}.${decimalPart.slice(0, 3)}`;
+    }
+    return str;
   };
 
   if (loading) {
